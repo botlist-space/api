3ea7e1830e8b463e8f90723d6d9fb5c5778b5e3f
Jacob Gunther <jacobgunther12@gmail.com>
2018-03-28 20:10:12 -0500

    Pushed 1.0.0

---
+++ examples/test.js
@@ -0,0 +1,27 @@
+const Client = require('../lib/Client');
+
+const client = new Client('307994108792799244', 'xxx');
+
+client.getAllBots().then(bots => {
+	console.log('The site has a total of ' + bots.length + ' bots.');
+}).catch((e) => {
+	console.error('Failed to get all bots. ' + e.code);
+});
+
+client.getBot('228537642583588864').then(bot => {
+	console.log('Vexera has ' + bot.count + ' servers.');
+}).catch((e) => {
+	console.error('Failed to get bot. ' + e.code);
+});
+
+client.getSelfBot().then(bot => {
+	console.log('The name of the current bot is ' + bot.name + '.');
+}).catch((e) => {
+	console.error('Failed to get self bot. ' + e.code);
+});
+
+client.postServerCount(250).then(() => {
+	console.log('Set the current bot\'s server count to 250 guilds.');
+}).catch((e) => {
+	console.error('Failed to post server count. ' + e.code);
+});
